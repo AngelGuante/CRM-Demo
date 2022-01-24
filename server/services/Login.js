42d42fc89ec151462bcaa1d1a1a7428709b59dc0
AngelGuante <miguel_guante@outlook.com>
2022-01-23 22:48:49 -0400

    Saving permissions and access to catching data

--- server/services/Login.js
+++ server/services/Login.js
@@ -7,13 +7,26 @@ const { SignedInUsers } = require('../Utils/staticsVariables.js');
 class LoginService {
     async SignIn(req) {
         const data = req.body;
-        const [user] = (await sequelize.query(`SELECT U.id, ES.name, C.companynumber, U.password, BO.id branch_office_id
-                                                   FROM "user" U
-                                                   JOIN employee E ON E.id = U.employee_id
-                                                   JOIN branch_office BO ON BO.id = E.branch_office_id
-                                                   JOIN company C ON C.id = BO.company_id
-                                                   JOIN employee_status ES ON ES.id = E.employee_status_id
-                                                   `))[0];
+        const [user] = (await sequelize.query(`SELECT U.id, ES.name, C.companynumber, U.password, BO.id branch_office_id, R.id rol_id,
+                                                    (SELECT string_agg(permission_id::text, ',')  permissions
+                                                    FROM Role_Permission_Access 
+                                                    WHERE role_id = R.id
+                                                    GROUP BY role_id
+                                                    ),
+                                                    (SELECT string_agg(access_id::text, ',')  access
+                                                    FROM Role_Permission_Access 
+                                                    WHERE role_id = R.id
+                                                    GROUP BY role_id
+                                                    )
+                                                    FROM "user" U
+                                                    JOIN employee E ON E.id = U.employee_id
+                                                    JOIN branch_office BO ON BO.id = E.branch_office_id
+                                                    JOIN company C ON C.id = BO.company_id
+                                                    JOIN employee_status ES ON ES.id = E.employee_status_id
+												    JOIN Job J ON J.id = E.job_id
+												    JOIN role R ON R.job_id = J.id
+                                                    WHERE U.name = '${data.user.toLowerCase()}'
+                                                    `))[0];
 
         if (user) {
             if (user['id'] !== 1)
@@ -40,8 +53,13 @@ class LoginService {
                     "token": `Bearer ${token}`,
                     "branch_office": user['branch_office_id'],
                     "company_id": user['id'],
-                    "companyNumber": user['companynumber']
+                    "rol_id": user['rol_id'],
+                    "companyNumber": user['companynumber'],
+                    "permissions": user['permissions'],
+                    "access": user['access'],
                 });
+
+                console.log(SignedInUsers);
 
                 return `{"status": 200, "title": "Success", "message": "${token}"}`;
             }
