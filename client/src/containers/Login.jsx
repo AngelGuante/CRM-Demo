cffcbd2003bd9a878967ab42e57456d8ec897f69
AngelGuante <miguel_guante@outlook.com>
2022-05-07 16:13:12 -0400

    adding login front structure

--- client/src/containers/Login.jsx
+++ client/src/containers/Login.jsx
@@ -1,11 +1,63 @@
+import { useState } from "react";
+
 const LoginContainer = () => {
+    const [form, setForm] = useState({
+        companynumber: '1000',
+        user: 'msuero2',
+        pass: '2109'
+    });
+
+    const handleIputChange = (event) => {
+        setForm({
+            ...form,
+            [event.target.name]: event.target.value
+        });
+    }
+
+    const Login = async (event) => {
+        event.preventDefault();
+        console.log(form)
+        let res = await fetch('http://localhost:3000/Api/login/Access', {
+            method: 'POST',
+            body: JSON.stringify(form),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        let json = await res.json();
+
+        console.log(json)
+    }
+
     return (
-        <div>
-            <input placeholder="company number" maxLength={6} />
-            <input placeholder="username" maxLength={15} />
-            <input placeholder="password" maxLength={30} type="password" />
-            <button>send</button>
-        </div>
+        <form onSubmit={Login}>
+            <input
+                value={form.companynumber}
+                name="companynumber"
+                placeholder="company number"
+                maxLength={6}
+                onChange={handleIputChange}
+            />
+            <input
+                value={form.user}
+                name="user"
+                placeholder="username"
+                maxLength={15}
+                onChange={handleIputChange}
+            />
+            <input
+                value={form.pass}
+                name="pass"
+                placeholder="password"
+                maxLength={30}
+                type="password"
+                onChange={handleIputChange}
+            />
+            <button type="submit">
+                send
+            </button>
+        </form>
     )
 }
 
